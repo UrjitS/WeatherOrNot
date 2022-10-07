3173ad66e990da6a3ae2b0cf616e1e9c30ea82c0
urjit <urjitsharma12@gmail.com>
2022-10-06 22:49:36 -0700

    Created login and signup wire-frames

--- app/src/main/java/com/example/weatherornot/MainActivity.java
+++ app/src/main/java/com/example/weatherornot/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.weatherornot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void continueAsGuest(View view) {
+        Intent continueAsGuest = new Intent(MainActivity.this, SearchPage.class);
+        startActivity(continueAsGuest);
+    }
+
+    public void loadSignUpPage(View view) {
+        Intent loadSignUpPage = new Intent(MainActivity.this, SignUpPage.class);
+        startActivity(loadSignUpPage);
+    }
+
+    public void loginButton(View view) {
+        Intent loginUser = new Intent(MainActivity.this, SearchPage.class);
+        startActivity(loginUser);
     }
 }
