bbfc637e87b1df4d7bfe2cc77446b2930cd3dec3
Ethan Diosana <danielrenendez@gmail.com>
2022-10-09 22:28:44 -0700

    Finished Search Page wireframe

--- app/src/main/java/com/example/weatherornot/SearchPage.java
+++ app/src/main/java/com/example/weatherornot/SearchPage.java
@@ -3,12 +3,28 @@ package com.example.weatherornot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SearchPage extends AppCompatActivity {
+
+    private Spinner weatherSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+
+        weatherSpinner = findViewById(R.id.searchPage_weatherSpinner);
+
+        populate_weatherSpinner();
+    }
+
+    /** Fills the weatherSpinner with different types of weather. */
+    private void populate_weatherSpinner() {
+        String[] weatherArray = getResources().getStringArray(R.array.weather_types);
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(SearchPage.this, android.R.layout.simple_spinner_item, weatherArray);
+        stringArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        weatherSpinner.setAdapter(stringArrayAdapter);
     }
 }
