d185af33b64e500becb631ee7470aa81acda08ae
urjit <urjitsharma12@gmail.com>
2022-10-22 13:09:14 -0700

    Updated login, signup pages

--- app/src/main/java/com/example/weatherornot/MainActivity.java
+++ app/src/main/java/com/example/weatherornot/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.weatherornot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
-
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent loginUser = new Intent(MainActivity.this, SearchPage.class);
+//            startActivity(loginUser);
+//        }
+    }
     public void continueAsGuest(View view) {
         Intent continueAsGuest = new Intent(MainActivity.this, SearchPage.class);
         startActivity(continueAsGuest);
@@ -26,7 +47,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginButton(View view) {
-        Intent loginUser = new Intent(MainActivity.this, SearchPage.class);
-        startActivity(loginUser);
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("log", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent loginUser = new Intent(MainActivity.this, SearchPage.class);
+                            startActivity(loginUser);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("log", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/weatherornot/SignUpPage.java
+++ app/src/main/java/com/example/weatherornot/SignUpPage.java
@@ -4,18 +4,75 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignUpPage extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+        mAuth = FirebaseAuth.getInstance();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent signUpUser = new Intent(SignUpPage.this, MainActivity.class);
+            startActivity(signUpUser);
+        }
+    }
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
+
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+
+    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+
+    public static boolean passwordIsValid(final String password) {
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
     public void signUpAccount(View view) {
-        Intent signUpUser = new Intent(SignUpPage.this, SearchPage.class);
-        startActivity(signUpUser);
+        EditText emailField = findViewById(R.id.signUpEmailField);
+        EditText passwordField = findViewById(R.id.signUpPassword);
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if (validate(email) && passwordIsValid(password)) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent signUpUser = new Intent(SignUpPage.this, SearchPage.class);
+                            startActivity(signUpUser);
+                        } else {
+                            Toast.makeText(SignUpPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+        } else {
+            Toast.makeText(SignUpPage.this, "Please enter valid email address and password.",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
