6e7fd86442232b480118c436af8e4b1b43182c5f
Prabh <prabhjeetsokhey@gmail.com>
2022-11-06 13:56:31 -0800

    google page appears (needs key to view still).

--- app/src/main/java/com/example/weatherornot/GoogleMap.java
+++ app/src/main/java/com/example/weatherornot/GoogleMap.java
@@ -1,14 +1,36 @@
 package com.example.weatherornot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GoogleMap extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMap extends AppCompatActivity implements OnMapReadyCallback {
+
+    SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull com.google.android.gms.maps.GoogleMap googleMap) {
+
+        // Marker test.
+        LatLng comp3717Lecture = new LatLng(49.25010954461797, -123.00275621174804);
+        googleMap.addMarker(new MarkerOptions().position(comp3717Lecture).title("COMP 3717 Lecture"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(comp3717Lecture, 15));
     }
 }
