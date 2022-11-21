9fa55c46b64c1df823a49dfd6c9b2da0bcbc2e56
Ethan Diosana <danielrenendez@gmail.com>
2022-11-21 12:41:34 -0800

    Changed some values to final. Changed some variable names.

--- app/src/main/java/com/example/weatherornot/SearchPage.java
+++ app/src/main/java/com/example/weatherornot/SearchPage.java
@@ -45,13 +45,15 @@ public class SearchPage extends AppCompatActivity implements NavigationView.OnNa
     Toolbar toolbar;
     FragmentManager fragmentManager;
     EditText stopNumber, routeNumber;
-    private final String estimatesUrl = "https://api.translink.ca/rttiapi/v1/stops";
-    private final String appId = "H6I5JajNoTKkm7Ub2Wj0";
+
+    private final String ESTIMATES_URL = "https://api.translink.ca/rttiapi/v1/stops";
+    private final String APP_ID = "H6I5JajNoTKkm7Ub2Wj0";
+
     boolean finishedSearch = false;
-    private ArrayList<String> busesDestination = new ArrayList<>();
-    private ArrayList<String> busesTime = new ArrayList<>();
-    private ArrayList<String> busPattern = new ArrayList<>();
-    private ArrayList<String> busLastUpdate = new ArrayList<>();
+    private final ArrayList<String> busesDestination = new ArrayList<>();
+    private final ArrayList<String> busesTime = new ArrayList<>();
+    private final ArrayList<String> busPattern = new ArrayList<>();
+    private final ArrayList<String> busLastUpdate = new ArrayList<>();
 //    private ArrayList<String> busDirection = new ArrayList<>();
 
 
@@ -157,7 +159,7 @@ public class SearchPage extends AppCompatActivity implements NavigationView.OnNa
                 // SUCCESS: The search term is a valid integer.
                 // Set the tempURL and proceed.
                 // https://api.translink.ca/rttiapi/v1/stops/60980/estimates
-                tempURL = estimatesUrl + "/" + stopNo + "/estimates?apikey=" + appId + "&routeNo=" + routeNo;
+                tempURL = ESTIMATES_URL + "/" + stopNo + "/estimates?apikey=" + APP_ID + "&routeNo=" + routeNo;
             } catch (NumberFormatException e) {
                 // FAILURE: The search term is not a valid integer.
                 final Toast t = Toast.makeText(getApplicationContext(), errorMSGRoute, Toast.LENGTH_LONG);
@@ -172,7 +174,7 @@ public class SearchPage extends AppCompatActivity implements NavigationView.OnNa
                 Integer.parseInt(stopNo);
                 // SUCCESS: The search term is a valid integer.
                 // Set the tempURL and proceed.
-                tempURL = estimatesUrl + "/" + stopNo + "/estimates?apikey=" + appId;
+                tempURL = ESTIMATES_URL + "/" + stopNo + "/estimates?apikey=" + APP_ID;
             } catch (NumberFormatException e) {
                 // FAILURE: The search term is not a valid integer.
                 final Toast t = Toast.makeText(getApplicationContext(), errorMSGRoute, Toast.LENGTH_LONG);
